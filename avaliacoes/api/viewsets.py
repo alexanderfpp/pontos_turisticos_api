2aeb513e7a7f37748e0ba17ce941f61087a7840a
Alexander Praxedes <alexanderpraxedes@hotmail.com>
2018-07-11 02:28:04 -0300

    Alteração de permissão para readonly

--- avaliacoes/api/viewsets.py
+++ avaliacoes/api/viewsets.py
@@ -3,7 +3,13 @@ from rest_framework.viewsets import ModelViewSet
 from avaliacoes.models import Avaliacao
 from .serializers import AvaliacaoSerializer
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 class AvaliacaoViewSet(ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
