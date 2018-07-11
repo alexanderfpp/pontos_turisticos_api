e844c7be5f03d02730c30b744535299a3e25879e
Alexander Praxedes <alexanderpraxedes@hotmail.com>
2018-07-11 02:05:17 -0300

    Alteração de permissão para readonly

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -3,8 +3,19 @@ from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Atracao
 from .serializers import AtracaoSerializer
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 class AtracaoViewSet(ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    # Apenas se estiver autenticado: IsAuthenticated
+    # Apenas se for administrador: IsAdminUser
+    # Autenticado ou acesso somente à leitura (GET): IsAuthenticatedOrReadOnly
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
     filter_backends = (DjangoFilterBackend,)
